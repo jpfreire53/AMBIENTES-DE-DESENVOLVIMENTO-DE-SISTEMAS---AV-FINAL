3d545f137d743441b6693b413a68e3b00b4894ac
Adilsonjvr <adilsonjvr@hotmail.com>
2023-06-01 21:41:09 -0300

    Soma

---
+++ src/Soma.java
@@ -0,0 +1,13 @@
+public class Soma {
+    public int soma(int a, int b) {
+        int resultado = a + b;
+        System.out.println("A soma de " + a + " e " + b + " é: " + resultado);
+        return resultado;
+    }
+
+    public int multiplica(int a, int b) {
+        int resultado = a * b;
+        System.out.println("A multiplicação de " + a + " e " + b + " é: " + resultado);
+        return resultado;
+    }
+}
