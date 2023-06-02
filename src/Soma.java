5b1a9a3c08993bb807a01fa2195999edce5b794b
Adilsonjvr <adilsonjvr@hotmail.com>
2023-06-01 22:09:06 -0300

    Calculadora

--- src/Soma.java
+++ src/Soma.java
@@ -5,9 +5,16 @@ public class Soma {
         return resultado;
     }
 
-    public int multiplica(int a, int b) {
+    public int multiplicacao(int a, int b) {
         int resultado = a * b;
         System.out.println("A multiplicação de " + a + " e " + b + " é: " + resultado);
         return resultado;
     }
+
+    public double potencia(double base, double expoente) {
+        double resultado = Math.pow(base, expoente);
+        System.out.println("A potência de " + base + " elevado a " + expoente + " é: " + resultado);
+        return resultado;
+    }
 }
+
